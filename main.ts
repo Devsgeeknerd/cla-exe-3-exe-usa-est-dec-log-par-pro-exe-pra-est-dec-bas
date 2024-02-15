a5e7aa01a5753058f999a130fe5310844a1e905b
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-15 15:15:32 -0300

    adicionando conteudo

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
+// Declaração de uma variável 'idadePessoa' do tipo number com valor 18.
 let idadePessoa: number = 18;
 
+// Verifica se a idade está na faixa de 0 a 19 anos.
 if (idadePessoa >= 0 && idadePessoa <= 19) {
   console.log("Sua faixa etária te classifica como uma pessoa jovem");
-} else if (idadePessoa > 19 && idadePessoa <= 59) {
+}
+// Se a idade não está na faixa de 0 a 19, verifica se está na faixa de 20 a 59 anos.
+else if (idadePessoa > 19 && idadePessoa <= 59) {
   console.log("Sua faixa etária te classifica como uma pessoa adulta");
-} else {
+}
+// Se a idade não está em nenhuma das faixas anteriores, considera como fase sênior.
+else {
   console.log(
     "Sua faixa etária te classifica como uma pessoa na fase sênior da vida"
   );
