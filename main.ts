de4367e1028aacc787013621682d04156fa9872c
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-16 14:12:04 -0300

    atualizando conteudo

--- main.ts
+++ main.ts
@@ -3,15 +3,20 @@ let idadePessoa: number = 18;
 
 // Verifica se a idade está na faixa de 0 a 19 anos.
 if (idadePessoa >= 0 && idadePessoa <= 19) {
+  // Se a condição for verdadeira, imprime uma mensagem indicando que a pessoa é jovem.
   console.log("Sua faixa etária te classifica como uma pessoa jovem");
 }
 // Se a idade não está na faixa de 0 a 19, verifica se está na faixa de 20 a 59 anos.
 else if (idadePessoa > 19 && idadePessoa <= 59) {
+  // Se a condição for verdadeira, imprime uma mensagem indicando que a pessoa é adulta.
   console.log("Sua faixa etária te classifica como uma pessoa adulta");
 }
 // Se a idade não está em nenhuma das faixas anteriores, considera como fase sênior.
 else {
+  // Se nenhuma das condições anteriores for verdadeira, imprime uma mensagem indicando que a pessoa está na fase sênior da vida.
   console.log(
     "Sua faixa etária te classifica como uma pessoa na fase sênior da vida"
   );
 }
+
+
